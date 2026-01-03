d15a9dc8f9be046c1c872dfd7eb6c98ac4e36053
SiddharthSingh <SiddharthSingh21122002@gmail.com>
2026-01-03 14:29:04 +0530

    Updated

--- Chapter_1/Introduction.java
+++ Chapter_1/Introduction.java
@@ -1,1 +1,71 @@
 // We understand the basic elements of algorithms, the importance of algorithm analysis.
+
+// 1.1 Variables.
+/* Before getting in to the definition of Variables, let us relate them to an old mathematical equation.
+   Many of us would have solved many mathematical equations since childhood. As an example, consider the
+   equation below:
+                                  x^2 + 2y - 2 = 1
+
+   The important thing that we need to understand is that the equation has names (x and y) which hold values
+   (data). That means the name (x and y) are placeholders for representing data. Similarly, in computer
+   science programming we need something for holding data, and variables in the way to do that.
+ */
+
+//-------------------------------------------------------------------------------------------------------------
+
+// 1.2 Data Types.
+/* Data type is the name used in computer science programming for this purpose. A data type in a programming
+   language is a set of data with predefined values. Examples of data types are: integer, floating point, unit
+   numbers, character, string, etc.
+
+   There are two types of Data types:-
+   a. System-defined data types(also called Primitive data types).
+      Data types that are defined by system are called primitive data types. The primitive data types provided
+      by many programming languages are: int, float, char,double, bool, etc. The numbers of bits allocated for
+      each primitive data type depends on the programming languages, the compiler and the operating system.
+
+   b. User-defined data types.
+      Most programming languages allow the users to define their own data types, called - defined data types.
+      For example:- classes in Java.
+ */
+
+//-------------------------------------------------------------------------------------------------------------
+
+// 1.3 Data Structure
+/* Data Structure is a particular way of storing and organizing data in a computer so that it can be used efficiently.
+   A data structure is special format for organizing and storing data. General data structure types include arrays,
+   files, linked lists, stacks, queues, trees, graphs and so on.
+
+   Depending on the organization of the elements, data structures are classified into teo types :-
+   a. Linear data structures:- Elements are accessed in a sequential order but it is not compulsory to store all
+      elements sequentially (say Linked Lists). Examples: Linked Lists, Stacks and Queues.
+   b. Non-Linear data Structures:- Elements of this data structure are stored/accessed in a non-linear order.
+      Examples: tress and graphs.
+ */
+
+//-------------------------------------------------------------------------------------------------------------
+
+// 1.4 Abstract Data Types (ADTs)
+/* The process of solving problems, we combine the data structures with their operations and we call this Abstract
+   Data Types (ADTs). An ADT consists of two parts:
+   1. Declaration of Data
+   2. Declaration of operations
+
+   Commonly used ADTs includes: Linked Lists, Stacks, Queues, Priority Queues, Binary Trees, Dictionaries, Disjoint
+   Sets(Union and find), hash tables, Graphs, and many others. for example, stack uses a LIFO (Last-In-First-Out)
+   mechanism while storing the data in data structures. The last element inserted into the stack is the first element
+   that gets deleted.
+   Common operations are: creating the stack, push an element onto the stack, pop an element from the stack, finding
+   the current top of the stack, finding the number of elements in the stack, etc.
+ */
+
+//-------------------------------------------------------------------------------------------------------------
+
+// 1.5 What is an Algorithm?
+/* An algorithm is the step-by-step unambiguous instructions to solve a given problem.
+   In the traditional study of algorithms, there are two main criteria for judging the merits of algorithms:
+   correctness(does the algorithm give solution to the problem iin a finite number of steps?) and efficiency(how much
+   resources in terms of memory and time) does it rake to execute it.
+
+   NOTE:- we do not have to prove each step of the algorithm.
+ */
