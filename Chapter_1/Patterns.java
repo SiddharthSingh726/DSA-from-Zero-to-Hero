caf5fec78965d14e2dfe0ea7cf8ca728f660d5fe
SiddharthSingh <SiddharthSingh21122002@gmail.com>
2026-01-05 06:37:48 +0530

    Done

---
+++ Chapter_1/Patterns.java
@@ -0,0 +1,148 @@
+// Patterns
+
+// 1. Prefix Sum
+/* Query sum of elements in a subarray.
+
+   Questions :- 303,525,560
+ */
+
+//-------------------------------------------------------------------------------------------------------------
+
+// 2. Two Pointers
+/* There are two variables that can move towards each other or go far from each other based on the problem.
+
+   Questions :- 167,15,11
+ */
+
+//-------------------------------------------------------------------------------------------------------------
+
+// 3. Sliding Window
+/* This help us find subarray or substring.
+
+   Questions :- 643,3,76
+ */
+
+// 4. Fast and Slow Pointers
+/* This solves the problems related to Linked List and Array which involves finding cycles. Moving two pointers at different
+   speeds.
+
+   questions :- 141,202,287
+ */
+
+//-------------------------------------------------------------------------------------------------------------
+
+// 5. Linked List In-place Reversal
+/* Do it in place without using extra space that where this pattern can help. We can use this for any problem that ask to reaarange
+the links between nodes of a linked list.
+
+   Question :- 206,92,24
+ */
+
+//-------------------------------------------------------------------------------------------------------------
+
+// 6. Monotonic Stack
+/* This pattern uses a stack to find the next greater or next smaller element in an Array.
+
+   Question :- 496,739,84
+ */
+
+// 7. Top 'K' Elements also do Quickselect
+/* This patterns helps to find the  'K' largest, 'K' smallest or 'K' most frequent elements in a dataset.
+   If the problem ask to find 'K' largest element than use ----> MIN-HEAP
+   if the problem ask to find 'K' smallest element than use -----> MAX-HEAP
+
+   Question :- 215,347,373
+ */
+
+//-------------------------------------------------------------------------------------------------------------
+
+// 8. Overlapping Intervals
+/* This pattern is useful for the problems involving INTERVAL or RANGE that may overlap.
+   Merging Interval, Interval Intersection , Insert Interval and Find Minimum Meeting Rooms.
+
+   Questions :- 65,67,135
+ */
+
+//-------------------------------------------------------------------------------------------------------------
+
+// 9. Modified Binary Search
+/* Modified Binary Search pattern is a variant of the standard binary search algorithm used to solve problems where the array
+   is not perfectly sorted like a. Searching in a nearly sorted array, Searching in  a rotated sorted array, Searching
+   in a list with unknown length, Searching in an Array with Duplicates, Finding the first or last occurrence of an element,
+   Finding the square root of a number, Finding a peak element.
+
+   Questions :- 33,153,240
+ */
+
+//-------------------------------------------------------------------------------------------------------------
+
+// 10. Binary Tree Traversal
+/* Binary trees are all about traversing it in a specific order. The four popular ways to traverse the binary tree are :-
+   a. Pre Order:- root->left->right
+   b. In Order:- left->root->right
+   c. Post Order:- left->right->root
+   d. Level order: level by level
+
+   To retrieve the values of the binary search tree in sorted order, use inorder traversal.
+   To create a copy of a tree (serialization), use preorder traversal.
+   When you want to process child nodes before the parent, use postorder traversal.
+   When you need to explore all nodes at current level before next, use level order traversal.
+
+   Questions :- 257,230,124,107
+ */
+
+//-------------------------------------------------------------------------------------------------------------
+
+// 11. Depth-First Search (DFS)
+/* It is used to explore all paths or branches in graphs or trees to solve problems like finding a path between to nodes,
+   Checking if a graph contains a cycle, Finding the topological order in a directed asy graph and counting the number of connected
+   components in a graph.
+
+   Questions :- 133,113,210
+ */
+
+//-------------------------------------------------------------------------------------------------------------
+/* 12. Breath-First Search (BFS)
+   BFS is a traversal technique that explore nodes level by level in a tree or graph. Its is very useful for problems like
+   finding the sortest path between two nodes in an unweighted graph, Printing all nodes of a tree level by level, Finding all
+   connected components in a graph, Finding the shortest transformation sequence from one word to other.
+
+  Questions :- 102,994,127
+ */
+
+//-------------------------------------------------------------------------------------------------------------
+
+// 13. Matrix Traversal
+/* Most matrix traversal problems can be seen as graph problems.In a graph you have nodes and edges. In a matrix which is
+   usually a 2D array, your nodes are the cells from where you can reach to adjacent cells horizontally, vertically or diagonally
+   depending on the problem. We can solve problems like finding the shortest path in a grid. We can solve matrix problems using
+   the DFS or BFS.
+
+   Questions :- 733, 200, 130
+ */
+
+//-------------------------------------------------------------------------------------------------------------
+
+// 14. Backtracking
+/* Backtracking is a powerful technique used to solve problems that involve exploring all potential solutions paths and backtracking
+   the paths that do not lead to a valid solution.
+   Questions we can solve are :- generate all possible permutations combinations of a given set of elements,
+   Solve puzzles like SUDOKU or N-QUEENS problems, Find all possible paths from starting to end in a graph or a maze,
+   Generate all valid parentheses of a given length.
+
+   Questions :-  46,78,51
+ */
+
+//-------------------------------------------------------------------------------------------------------------
+
+// 15. Dynamic Programming
+/* DP is a power full technique used for solving optimization problems by breaking them down  into smaller sub-problems and
+   storing their solutions to avoid repetitive work.
+   It is particularly useful for problems with overlapping subproblems and optimal substructure properties like problems when
+   you need to maximize or minimize a certain value and counting problems where you need to count the number of ways to achieve
+   a certain goal.
+   Important patterns of DP :- Fibonacci Numbers, 0/1 Knapsack, Longest Common Subsequence (LCS), Longest Increasing Subsequence (LIS),
+   Subset Sum and Matrix Chain Multiplication.
+
+   Questions :- 70,300,322,416,1143,312
+ */
