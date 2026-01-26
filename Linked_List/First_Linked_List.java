10b51bfea0419f40aa0975822fc86f785ce89902
SiddharthSingh <SiddharthSingh21122002@gmail.com>
2026-01-26 15:48:02 +0530

    Updated

---
+++ Linked_List/First_Linked_List.java
@@ -0,0 +1,19 @@
+package Linked_List;
+
+class Node{
+    int data;
+    Node next;
+    Node(int data,Node next){
+        this.data=data;
+        this.next=null;
+    }
+class LinkedList {
+    Node head;   // head of the list
+
+    LinkedList() {
+        head = null; // empty list
+    }
+}
+public class First_Linked_List {
+    }
+}
