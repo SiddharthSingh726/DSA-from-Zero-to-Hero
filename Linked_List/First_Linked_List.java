9cc24971202529212a39c70dc74a39df479638b5
SiddharthSingh <SiddharthSingh21122002@gmail.com>
2026-01-29 23:31:22 +0530

    Updated

--- Linked_List/First_Linked_List.java
+++ Linked_List/First_Linked_List.java
@@ -1,19 +1,26 @@
 package Linked_List;
 
-class Node{
+class Node {
     int data;
     Node next;
-    Node(int data,Node next){
-        this.data=data;
-        this.next=null;
-    }
-class LinkedList {
-    Node head;   // head of the list
-
-    LinkedList() {
-        head = null; // empty list
-    }
 }
 public class First_Linked_List {
+    public static void main(String[] args) {
+        Node head = new Node();
+        Node n1 = new Node();
+        Node n2 = new Node();
+
+        head.data = 1;
+        n1.data = 2;
+        n2.data = 3;
+
+        head.next = n1;
+        n1.next = n2;
+        n2.next = null;
+
+
+        for(Node temp = head; temp != null; temp = temp.next ) {
+            System.out.print(temp.data + " ");
+        }
     }
 }
